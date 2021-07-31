d93bb1facd6117278b7b405993f3a2dd3e69060e
Ajay Titus <ajaytitus1386@gmail.com>
2021-07-31 12:39:48 +0530

    Started contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,12 +3,20 @@ import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 
 import '../styles/home.scss'
+import * as contactStyles from '../styles/contact.module.scss'
+
 export default function Contact() {
     return (
         <Layout>
         <Navbar />       
         <div className="main">
-            <h1>Contact Page</h1>
+            <div className={contactStyles.container}>
+                <h1>Interested in Getting in Touch?</h1>
+                <div className={contactStyles.divider}></div>
+                <p className={contactStyles.text}>
+                    Have any inquires, feedback, suggestions or just have anything to say? Feel free to direct any relevant topics via email.
+                </p>
+            </div>
         </div>
         </Layout>
     )
