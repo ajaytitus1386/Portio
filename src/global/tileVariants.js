259e300a8bdaedf17f9c1386958dc86d8252f9ad
Ajay Titus <ajaytitus1386@gmail.com>
2022-09-30 16:13:48 +0530

    style: remove pop out effect

--- src/global/tileVariants.js
+++ src/global/tileVariants.js
@@ -14,8 +14,7 @@ const tileVariants = {
     },
     hover : {
         boxShadow: "0px 0px 10px 10px rgba(0,0,0,0.2)",
-        scale : 1.05,
-        y: -20
+       
     },
     hoverCenter : {
         boxShadow: "0px 0px 10px 10px rgba(0,0,0,0.2)",
