4d70dfe3d4ba196150e1ca73da1131707f8c2fa4
Ajay Titus <ajaytitus1386@gmail.com>
2022-10-03 11:18:40 +0530

    feat: remake section heading

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -1,13 +1,84 @@
-import React from 'react'
-import '../styles/home.scss'
+import React, { useEffect } from "react"
+import { motion, useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer"
 
-export default function SectionHeading({headingText}) {
-    return (
-        <div>
-            <div className={"sectionHeading"} >
-                  {headingText}
-            </div>
-            <div className="divider"></div>
-        </div>
-    )
+import * as sectionHeadingStyles from "../styles/sectionheading.module.scss"
+
+const parentVariants = {
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.1,
+      staggerChildren: 0.6,
+    },
+  },
+}
+
+const divderVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 0.8,
+    transition: {
+      opacity: {
+        delay: 0.6,
+        duration: 0.4,
+      },
+    },
+  },
+}
+
+const textVariants = {
+  hidden: {
+    y: -40,
+    rotate: "inherit",
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 0.8,
+    transition: {
+      duration: 0.4,
+    },
+  },
+}
+
+export default function SectionHeading({ headingText }) {
+  const controls = useAnimation()
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  })
+
+  useEffect(() => {
+    if (inView) controls.start("show")
+    //if (!inView) controls.start("hidden")
+  }, [controls, inView])
+
+  return (
+    <motion.div
+      variants={parentVariants}
+      initial="hidden"
+      animate={controls}
+      className={sectionHeadingStyles.container}
+      ref={ref}
+    >
+      <motion.div
+        variants={divderVariants}
+        initial="hidden"
+        animate="show"
+        className={sectionHeadingStyles.divider}
+      ></motion.div>
+      <motion.div
+        variants={textVariants}
+        initial="hidden"
+        animate="show"
+        className={sectionHeadingStyles.sectionHeading}
+      >
+        {headingText}
+      </motion.div>
+    </motion.div>
+  )
 }
