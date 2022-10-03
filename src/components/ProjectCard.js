6f3fe50e929b8d82d4c839ac3a6f6c352e0a592e
Ajay Titus <ajaytitus1386@gmail.com>
2022-10-03 19:13:15 +0530

    chore: format code

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,35 +1,39 @@
-import React from 'react'
-import * as cardStyles from '../styles/projectcard.module.scss';
-import {motion} from 'framer-motion';
+import React from "react"
+import * as cardStyles from "../styles/projectcard.module.scss"
+import { motion } from "framer-motion"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { tileVariants } from "../global/tileVariants";
+import { tileVariants } from "../global/tileVariants"
 import parse from "html-react-parser"
-import { Link } from 'gatsby';
+import { Link } from "gatsby"
 
-function ProjectCard({project}) {
-    console.log(project.frontmatter.url)
+function ProjectCard({ project }) {
   return (
-    <Link className={`${cardStyles.card} ${project.frontmatter.url ? cardStyles.urlActive : ""}`} to={project.frontmatter.url}>
-              <motion.div
-                className={cardStyles.tile}
-                variants={tileVariants}
-                initial="initial"
-                animate="visible"
-                whileHover="hover"
-              >
-                <div className={cardStyles.tileInfo}>
-                  <h2>{project.frontmatter.title}</h2>
-                  <h3>{project.frontmatter.stack}</h3>
-                </div>
-                <div className={cardStyles.tileImage}>
-                  <GatsbyImage
-                    image={getImage(project.frontmatter.thumb)}
-                    alt={project.frontmatter.title}
-                  />
-                </div>
-              </motion.div>
-              <div className={cardStyles.markdown}>{parse(project.html)}</div>
-            </Link>
+    <Link
+      className={`${cardStyles.card} ${
+        project.frontmatter.url ? cardStyles.urlActive : ""
+      }`}
+      to={project.frontmatter.url}
+    >
+      <motion.div
+        className={cardStyles.tile}
+        variants={tileVariants}
+        initial="initial"
+        animate="visible"
+        whileHover="hover"
+      >
+        <div className={cardStyles.tileInfo}>
+          <h2>{project.frontmatter.title}</h2>
+          <h3>{project.frontmatter.stack}</h3>
+        </div>
+        <div className={cardStyles.tileImage}>
+          <GatsbyImage
+            image={getImage(project.frontmatter.thumb)}
+            alt={project.frontmatter.title}
+          />
+        </div>
+      </motion.div>
+      <div className={cardStyles.markdown}>{parse(project.html)}</div>
+    </Link>
   )
 }
 
